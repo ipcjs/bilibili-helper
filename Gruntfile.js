cda370a626b9734f7abc1f40c390764dae608fbf
Ruo <jjj201200@163.com>
2016-06-26 21:06:59 +0800

    update the style of 'chat' & separate style in css file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,63 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-                mangle:true,
+                mangle: true,
                 sourceMap: false,
                 sourceMapName: 'dest/srcmap.map'
             },
             release: {
                 files: [{
-                    expand:true,
+                    expand: true,
                     cwd: 'src',
-                    src: ['**/*.js','!**/*.min.js'],
+                    src: ['**/*.js', '!**/*.min.js'],
                     ext: '.min.js',
                     dest: 'dest/'
                 }]
             }
         },
         htmlmin: {
-          main: {
-            options: {
-              collapseWhitespace: true,
-              minifyCSS: true,
-              minifyJS: true,
-              removeComments: true,
+            main: {
+                options: {
+                    collapseWhitespace: true,
+                    minifyCSS: true,
+                    minifyJS: true,
+                    removeComments: true,
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['**/*.html'],
+                    dest: 'dest/',
+                    ext: '.min.html'
+                }]
             },
-            files: [{
-                expand:true,
-                cwd: 'src',
-                src: ['**/*.html'],
-                dest: 'dest/'
-            }]
-          },
+        },
+        cssmin: {
+            main: {
+                options: {
+                    shorthandCompacting: false,
+                    roundingPrecision: -1
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['**/*.css', '!**/*.min.css'],
+                    dest: 'dest/',
+                    ext: '.min.css'
+                }]
+            }
         },
         copy: {
             main: {
                 files: [{
                     expand: true,
-                    cwd:'src',
-                    src: ['**/**','!**/*.html','!**/*.js','**/*.min.js','_locales/**'],
+                    cwd: 'src',
+                    src: ['**/**', '!**/*.html', '!**/*.js', '**/*.min.js', '!**/*.css', '**/*.min.css', '_locales/**'],
                     dest: 'dest/'
-              }]
+                }]
             }
         },
         compress: {
@@ -54,15 +70,16 @@ module.exports = function(grunt){
                     cwd: 'dest/',
                     src: ['**/*'],
                     dest: '/'
-                 }]
+                }]
             }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-compress');
 
-    grunt.registerTask('default', ['uglify:release','htmlmin:main','copy:main','compress:main']);
-    grunt.registerTask('debug', ['uglify:release','htmlmin:main','copy:main']);
+    grunt.registerTask('default', ['uglify:release', 'htmlmin:main', 'cssmin:main', 'copy:main', 'compress:main']);
+    grunt.registerTask('debug', ['uglify:release', 'htmlmin:main', 'cssmin:main', 'copy:main']);
 }
