0c01e3057619fedd3c34127f44f2df3ded67ec44
ipcjs <gipcjs@gmail.com>
2016-12-14 07:55:37 +0800

    suppoet Tampermonkey

--- bilibili_comment_search.js
+++ bilibili_comment_search.js
@@ -1,49 +1,61 @@
-// 在匿名函数中执行代码, 防止污染全局变量
+// ==UserScript==
+// @name         Bilibili评论定位
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  点击消息中心的评论后, 自动定位评论
+// @author       ipcjs
+// @match        http://*.bilibili.com/*?*aid=*
+// @grant        none
+// ==/UserScript==
+
 (function () {
     'use strict';
-    var id, type, feedback, group, title = 'bilibili_comment_search';
-    if (window.aid) {
-        id = window.aid;
-        type = "arc";
-    } else if (window.tp_id) {
-        id = window.tp_id;
-        type = "topic";
-    } else if (group = window.location.href.match(/aid=(\d+)/)) {
-        // 试图从url中提取id
-        id = group[1];
-        type = 'arc';
-    }
-    if (id) {
-        console.log(`${title}...`);
-        $('#comment .comm').children().remove(); // 移除评论区域..., 让bbFeedback重新生成
-        feedback = new bbFeedback(".comm", type, {autoLoad: true});
-        $("#load_comment").off("click").removeAttr("onclick").on("click", function () {
-            feedback.show(id, 1)
-        });
-        var replyId,
-            callback = function (context) {
-                var replyElement = $("#l_id_" + replyId, context);
-                if (replyElement.length != 0) {
-                    setTimeout(function () {
-                        $(document).scrollTop(replyElement.offset().top - 20)
-                    }, 0);
-                    if (replyElement.parents(".reply").length) {
-                        $(".re_ta", replyElement).click();
-                    } else {
-                        $(".huifu", replyElement).click()
-                    }
-                }
-            };
-        if (group = window.location.href.match(/#fb,([0-9]+),([0-9]+),([0-9]+),(.+)$/)) {
-            replyId = group[3];
-            feedback.show(id, 1, replyId, callback);
-        } else if (group = window.location.href.match(/#reply([0-9]+)$/)) {
-            replyId = group[1];
-            feedback.show(id, 1, replyId, callback);
-        } else {
-            feedback.show(id, 1);
+    function jumpToComment() {
+        var id, type, feedback, group, title = 'bilibili_comment_search';
+        if (window.aid) {
+            id = window.aid;
+            type = "arc";
+        } else if (window.tp_id) {
+            id = window.tp_id;
+            type = "topic";
+        } else if (group = window.location.href.match(/aid=(\d+)/)) {
+            // 试图从url中提取id
+            id = group[1];
+            type = 'arc';
         }
-    } else {
-        alert(`${title}在当前页面不可用`);
+        if (id) {
+            console.log(`${title}...`);
+            $('#comment .comm').children().remove(); // 移除评论区域..., 让bbFeedback重新生成
+            feedback = new bbFeedback(".comm", type, {autoLoad: true});
+            $("#load_comment").off("click").removeAttr("onclick").on("click", function () {
+                feedback.show(id, 1)
+            });
+            var replyId,
+                callback = function (context) {
+                    var replyElement = $("#l_id_" + replyId, context);
+                    if (replyElement.length !== 0) {
+                        setTimeout(function () {
+                            $(document).scrollTop(replyElement.offset().top - 20)
+                        }, 0);
+                        if (replyElement.parents(".reply").length) {
+                            $(".re_ta", replyElement).click();
+                        } else {
+                            $(".huifu", replyElement).click()
+                        }
+                    }
+                };
+            if (group = window.location.href.match(/#fb,([0-9]+),([0-9]+),([0-9]+),(.+)$/)) {
+                replyId = group[3];
+                feedback.show(id, 1, replyId, callback);
+            } else if (group = window.location.href.match(/#reply([0-9]+)$/)) {
+                replyId = group[1];
+                feedback.show(id, 1, replyId, callback);
+            } else {
+                feedback.show(id, 1);
+            }
+        } else {
+            console.log(`${title}在当前页面不可用`);
+        }
     }
+    $(window).load(jumpToComment); // 页面完全加载完成后在再触发
 })();
