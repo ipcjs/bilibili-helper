00bdd9d1c4635e6896adeda0feff0b8b83134f1d
ipcjs <gipcjs@gmail.com>
2016-12-21 07:13:36 +0800

    add note:xxx

--- bilibili_bangumi_aera_limit_hack.user.js
+++ bilibili_bangumi_aera_limit_hack.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         解除B站区域限制
 // @namespace    http://tampermonkey.net/
-// @version      1.4
-// @description  把获取视频地址相关接口的返回值替换成我的反向代理服务器的返回值
+// @version      1.5
+// @description  把获取视频地址相关接口的返回值替换成我的反向代理服务器的返回值; 因为替换值的操作是同步的, 所有会卡几下..., 普通视频不受影响; 我的服务器有点渣, 没获取成功请多刷新几下; 当前只支持bangumi.bilibili.com域名下的番剧视频; 
 // @author       ipcjs
 // @include      http://bangumi.bilibili.com/anime/*
 // @include      http://bangumi.bilibili.com/anime/v/*
