cd23c32374062c0cd5f75161ae3d048ce0c79961
ipcjs <gipcjs@gmail.com>
2016-12-21 20:02:58 +0800

    use unsafeWindow

--- bilibili_comment_search.user.js
+++ bilibili_comment_search.user.js
@@ -1,37 +1,36 @@
 // ==UserScript==
 // @name         Bilibili评论定位
 // @namespace    http://tampermonkey.net/
-// @version      0.6
-// @description  点击消息中心的评论后, 自动定位评论
+// @version      0.7
+// @description  点击消息中心的评论后, 自动定位评论; (B站评论系统改版中, 当前在番剧页面无效, B萌界面还是有效的)
 // @author       ipcjs
-// @match        http://*.bilibili.com/*?*aid=*
+// @include      http://*.bilibili.com/*?*aid=*
+// @include      http://message.bilibili.com/
 // @exclude      http://www.bilibili.com/html/html5player.html*
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        unsafeWindow
 // ==/UserScript==
-
-/**
-* 文档注释会被自动读取？
-*/
 
 (function () {
     'use strict';
     function jumpToComment() {
         var id, type, feedback, group, title = '[' + GM_info.script.name + ']';
-        if (window.aid) {
-            id = window.aid;
+        if (unsafeWindow.aid) {
+            id = unsafeWindow.aid;
             type = "arc";
-        } else if (window.tp_id) {
-            id = window.tp_id;
+        } else if (unsafeWindow.tp_id) {
+            id = unsafeWindow.tp_id;
             type = "topic";
-        } else if (group = window.location.href.match(/aid=(\d+)/)) {
+        } else if (group = unsafeWindow.location.href.match(/aid=(\d+)/)) {
             // 试图从url中提取id
             id = group[1];
             type = 'arc';
         }
-        if (id && window.bbFeedback) {
+        if (id && unsafeWindow.bbFeedback) {
             console.log(title, 'search...');
             $('#comment .comm').children().remove(); // 移除评论区域..., 让bbFeedback重新生成
-            feedback = new window.bbFeedback(".comm", type, {autoLoad: true});
+            feedback = new unsafeWindow.bbFeedback(".comm", type, {autoLoad: true});
             $("#load_comment").off("click").removeAttr("onclick").on("click", function () {
                 feedback.show(id, 1)
             });
@@ -49,23 +48,23 @@
                         }
                     }
                 };
-            if (group = window.location.href.match(/#fb,([0-9]+),([0-9]+),([0-9]+),(.+)$/)) {
+            if (group = unsafeWindow.location.href.match(/#fb,([0-9]+),([0-9]+),([0-9]+),(.+)$/)) {
                 replyId = group[3];
                 feedback.show(id, 1, replyId, callback);
-            } else if (group = window.location.href.match(/#reply([0-9]+)$/)) {
+            } else if (group = unsafeWindow.location.href.match(/#reply([0-9]+)$/)) {
                 replyId = group[1];
                 feedback.show(id, 1, replyId, callback);
             } else {
                 feedback.show(id, 1);
             }
         } else {
-            console.log(title, '当前页不可用:', window.location.href);
+            console.log(title, '当前页不可用:', unsafeWindow.location.href);
         }
     }
 
     if (document.readyState === 'complete') {
         jumpToComment();
     } else {
-        $(window).load(jumpToComment); // 页面完全加载完成后在再触发
+        $(unsafeWindow).load(jumpToComment); // 页面完全加载完成后在再触发
     }
 })();
