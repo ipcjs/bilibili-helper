2886f9c57f9da3f3c8b9b64b74da83f4309553dc
ipcjs <gipcjs@gmail.com>
2016-12-22 01:10:09 +0800

    Fix comment targeting on bangumi page.

--- bilibili_comment_search.user.js
+++ bilibili_comment_search.user.js
@@ -1,28 +1,84 @@
 // ==UserScript==
 // @name         Bilibili评论定位
 // @namespace    http://tampermonkey.net/
-// @version      0.7
-// @description  点击消息中心的评论后, 自动定位评论; (B站评论系统改版中, 当前在番剧页面无效, B萌界面还是有效的)
+// @version      0.8
+// @description  点击消息中心的评论后, 自动定位评论; (Update: 修复番剧页面跳转问题: 通过监听消息页面的点击事件获取真实原来的url, 需要等到消息页面载入完成后才有效;)
 // @author       ipcjs
 // @include      http://*.bilibili.com/*?*aid=*
+// @include      http://bangumi.bilibili.com/anime/*/play
 // @include      http://message.bilibili.com/
 // @exclude      http://www.bilibili.com/html/html5player.html*
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
 // @grant        unsafeWindow
 // ==/UserScript==
 
 (function () {
     'use strict';
+    function msgClickListener() {
+        var group;
+        if (group = this.href.match(/^http:\/\/bangumi\.bilibili\.com\/anime\/v\/(\d+)\?aid=(\d+)#reply(\d+)$/)) {
+            GM_setValue('bangumi_id=' + group[1], this.href); // 点击番剧页面的链接时存储真实的url
+            console.log('click', this.href);
+        }
+    }
+
+    function listenMsgClick() {
+        var a = $('#message_right ul > li > .message-main-right')
+            .find('> .message-content-title > a , > div > span.message-context > a')
+            .off('click', msgClickListener) // 先移除, 在监听
+            .click(msgClickListener);
+        console.log('listen click:', a.length, a);
+    }
+
+    function listenDomChange() {
+        console.log('listen DOM change.', 'readyState:', unsafeWindow.document.readyState);
+        new unsafeWindow.MutationObserver(function (mutations, observer) {
+            // console.log(...mutations);
+            mutations.forEach(function (item, index) {
+                if (item.type === 'childList'
+                    && item.target.nodeName === 'UL' && item.target.className === 'message-main-lists' // target为ul.message-main-lists
+                    && item.addedNodes.length > 10 // 添加消息列表的事件中, 添加的node数量一般为41
+                ) {
+                    // console.log(item, index);
+                    listenMsgClick();
+                }
+            });
+        }).observe(unsafeWindow.document.querySelector('#message_center_box'), {
+            childList: true,
+            subtree: true,
+            // attributes: true
+        });
+    }
+
+
     function jumpToComment() {
-        var id, type, feedback, group, title = '[' + GM_info.script.name + ']';
+        if (unsafeWindow.location.host === 'message.bilibili.com' && unsafeWindow.location.pathname === '/') {
+            listenMsgClick();
+            listenDomChange();
+        }
+
+        var id, type, feedback, group, valueName, temp,
+            title = '[' + GM_info.script.name + ']',
+            realUrl = unsafeWindow.location.href;
+        if (group = realUrl.match(/\/anime\/\d+\/play#(\d+)/)) { // 若为番剧页面
+            valueName = 'bangumi_id=' + group[1];
+            if (temp = GM_getValue(valueName)) { // 读取存储的真实url
+                realUrl = temp;
+                GM_deleteValue(valueName); // 读取成功后删除这个值
+                console.log('readUrl:', realUrl, 'GM_listValues', GM_listValues());
+            }
+        }
+
         if (unsafeWindow.aid) {
             id = unsafeWindow.aid;
             type = "arc";
         } else if (unsafeWindow.tp_id) {
             id = unsafeWindow.tp_id;
             type = "topic";
-        } else if (group = unsafeWindow.location.href.match(/aid=(\d+)/)) {
+        } else if (group = realUrl.match(/aid=(\d+)/)) {
             // 试图从url中提取id
             id = group[1];
             type = 'arc';
@@ -48,21 +104,21 @@
                         }
                     }
                 };
-            if (group = unsafeWindow.location.href.match(/#fb,([0-9]+),([0-9]+),([0-9]+),(.+)$/)) {
+            if (group = realUrl.match(/#fb,([0-9]+),([0-9]+),([0-9]+),(.+)$/)) {
                 replyId = group[3];
                 feedback.show(id, 1, replyId, callback);
-            } else if (group = unsafeWindow.location.href.match(/#reply([0-9]+)$/)) {
+            } else if (group = realUrl.match(/#reply([0-9]+)$/)) {
                 replyId = group[1];
                 feedback.show(id, 1, replyId, callback);
             } else {
                 feedback.show(id, 1);
             }
         } else {
-            console.log(title, '当前页不可用:', unsafeWindow.location.href);
+            console.log(title, '当前页不可定位:', unsafeWindow.location.href);
         }
     }
 
-    if (document.readyState === 'complete') {
+    if (unsafeWindow.document.readyState === 'complete') {
         jumpToComment();
     } else {
         $(unsafeWindow).load(jumpToComment); // 页面完全加载完成后在再触发
