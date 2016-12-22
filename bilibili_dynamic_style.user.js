76234fbac5f4532e56d797d963ebf8e5745702ea
ipcjs <gipcjs@gmail.com>
2016-12-23 05:45:12 +0800

    update to 0.6

--- bilibili_dynamic_style.user.js
+++ bilibili_dynamic_style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         B站主页动态提醒直接显示在顶栏
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  可以直接看到当前的提醒的类型, 省去鼠标移过去的麻烦...
 // @author       ipcjs
 // @match        http://www.bilibili.com/
