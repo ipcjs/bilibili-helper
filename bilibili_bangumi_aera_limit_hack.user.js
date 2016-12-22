7672fe9e669f78e1f4e1d17ccf9912925be6d955
ipcjs <gipcjs@gmail.com>
2016-12-22 21:46:46 +0800

    fix issue on sougou explorer

--- bilibili_bangumi_aera_limit_hack.user.js
+++ bilibili_bangumi_aera_limit_hack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         解除B站区域限制
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  把获取视频地址相关接口的返回值替换成我的反向代理服务器的返回值; 因为替换值的操作是同步的, 所有会卡几下..., 普通视频不受影响; 我的服务器有点渣, 没获取成功请多刷新几下; 当前只支持bangumi.bilibili.com域名下的番剧视频; 
 // @author       ipcjs
 // @include      http://bangumi.bilibili.com/anime/*
@@ -102,7 +102,7 @@
                                     }
                                 });
                                 if (obj.durl.length === 1 && obj.durl[0].length == 15126 && obj.durl[0].size === 124627) {
-                                    if (confirm('试图获取视频地址失败, 请登录biliplus' +
+                                    if (unsafeWindow.confirm('试图获取视频地址失败, 请登录biliplus' +
                                             '\n注意: 只支持"使用bilibili账户密码进行登录"'
                                         )) {
                                         unsafeWindow.top.location = biliplusHost + '/login';
