7a98ad1575813c2eb4226e222a7f8e3a6a993b95
ipcjs <gipcjs@gmail.com>
2016-12-29 03:27:41 +0800

    comment font is set to normal

--- underscore_toggle_sidebar.user.js
+++ underscore_toggle_sidebar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Toggle Sidebar For underscorejs.org & liaoxuefeng
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  RT.
 // @author       ipcjs
 // @include      http://underscorejs.org/
@@ -11,84 +11,84 @@
 // @require      https://rawgit.com/jakiestfu/Snap.js/develop/snap.min.js
 // ==/UserScript==
 
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	var  defaultConfig = {
-		sidebar: '#sidebar', container: 'body > .container', // element selector
-		maxPosition: 266, minPosition: -266, // Snap setting
-		top: '0px', width: '200px', // sidebar attr
-		marginLeft: '260px', // container attr
-		func: null // to run funcation
-	}, configMap = {
-		'underscorejs.org': defaultConfig,
-		'learningcn.com': defaultConfig,
-		'www.liaoxuefeng.com': {
-			sidebar: '#main .x-sidebar-left', container: '#main .x-center', 
-			maxPosition: 270, minPosition: -270,
-			top: '48px', width: '216px',
-			marginLeft: '220px', 
-			func: function(){
-				document.querySelector('#footer').style.display = 'none';
-				var id = window.location.pathname.split('/').reverse()[0];
-				$(sidebar).scrollTop($('#' + id).offset().top - $(sidebar).offset().top);
-			}
-		}
-	}, config = configMap[location.host];
+    var defaultConfig = {
+        sidebar: '#sidebar', container: 'body > .container', // element selector
+        maxPosition: 266, minPosition: -266, // Snap setting
+        top: '0px', width: '200px', // sidebar attr
+        marginLeft: '260px', // container attr
+        func: null // to run funcation
+    }, configMap = {
+        'underscorejs.org': defaultConfig,
+        'learningcn.com': defaultConfig,
+        'www.liaoxuefeng.com': {
+            sidebar: '#main .x-sidebar-left', container: '#main .x-center',
+            maxPosition: 270, minPosition: -270,
+            top: '48px', width: '216px',
+            marginLeft: '220px',
+            func: function () {
+                document.querySelector('#footer').style.display = 'none';
+                var id = window.location.pathname.split('/').reverse()[0];
+                $(sidebar).scrollTop($('#' + id).offset().top - $(sidebar).offset().top);
+            }
+        }
+    }, config = configMap[location.host];
 
-	var toggleBtn,
-		sidebar = document.querySelector(config.sidebar),
-		container = document.querySelector(config.container),
-		snapper = new Snap({
-			element: sidebar,
-			maxPosition: config.maxPosition,
-			minPosition: config.minPosition
-		}),
-		body = document.querySelector('body'),
-		marginLeft = container.style.marginLeft,
-		position = container.style.position;
-	console.log(sidebar, container);
+    var toggleBtn,
+        sidebar = document.querySelector(config.sidebar),
+        container = document.querySelector(config.container),
+        snapper = new Snap({
+            element: sidebar,
+            maxPosition: config.maxPosition,
+            minPosition: config.minPosition
+        }),
+        body = document.querySelector('body'),
+        marginLeft = container.style.marginLeft,
+        position = container.style.position;
+    console.log(sidebar, container);
 
-	/* set container attr */
-	container.style.marginLeft = config.marginLeft;
-	snapper.on('open', function(){ // to close...
-		container.style.marginLeft = '0px';
-		container.style.position = 'absolute';
-	});
-	snapper.on('close', function(){ // to open...
-		// container.style.marginLeft = config.maxPosition;
-		// container.style.marginLeft = marginLeft;
-		container.style.marginLeft = config.marginLeft;
-		container.style.position = position;
-	});
+    /* set container attr */
+    container.style.marginLeft = config.marginLeft;
+    snapper.on('open', function () { // to close...
+        container.style.marginLeft = '0px';
+        container.style.position = 'absolute';
+    });
+    snapper.on('close', function () { // to open...
+        // container.style.marginLeft = config.maxPosition;
+        // container.style.marginLeft = marginLeft;
+        container.style.marginLeft = config.marginLeft;
+        container.style.position = position;
+    });
 
-	/* set sidebar attr */
-	sidebar.style.display = 'block';
-	sidebar.style.width = config.width;
-	sidebar.style.float = 'left';
-	sidebar.style.overflowY = 'auto';
-	sidebar.style.top = config.top;
-	sidebar.style.bottom = '0px';
-	sidebar.style.position = 'fixed';
+    /* set sidebar attr */
+    sidebar.style.display = 'block';
+    sidebar.style.width = config.width;
+    sidebar.style.float = 'left';
+    sidebar.style.overflowY = 'auto';
+    sidebar.style.top = config.top;
+    sidebar.style.bottom = '0px';
+    sidebar.style.position = 'fixed';
 
-	/* set toggleBtn attr */
-	toggleBtn = document.createElement('a');
-	toggleBtn.id = 'toggle_btn';
-	toggleBtn.innerText = '>';
-	body.appendChild(toggleBtn);
-	toggleBtn.addEventListener('click', function(){
-		if( snapper.state().state=="right" ){
-			snapper.close();
-		} else {
-			snapper.open('right');
-		}
+    /* set toggleBtn attr */
+    toggleBtn = document.createElement('a');
+    toggleBtn.id = 'toggle_btn';
+    toggleBtn.innerText = '>';
+    body.appendChild(toggleBtn);
+    toggleBtn.addEventListener('click', function () {
+        if (snapper.state().state == "right") {
+            snapper.close();
+        } else {
+            snapper.open('right');
+        }
 
-	});
-	
+    });
+
     /* other */
-	config.func && config.func();
-	let style = document.createElement('style');
-	style.innerHTML = `
+    config.func && config.func();
+    let style = document.createElement('style');
+    style.innerHTML = `
 /* css.start */
 #toggle_btn {
     position: fixed;
@@ -108,7 +108,10 @@
     color: white;
     text-decoration: none;
 }
+pre .comment {
+    font-style: normal;
+}
 /* css.end */
     `;
-	document.querySelector('head').appendChild(style);
+    document.querySelector('head').appendChild(style);
 })();
