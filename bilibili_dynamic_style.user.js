cbed958e0894aff86a731949c41e21b9e1a93282
ipcjs <gipcjs@gmail.com>
2017-01-05 19:52:16 +0800

    delay pop "我的大会员" window.

--- bilibili_dynamic_style.user.js
+++ bilibili_dynamic_style.user.js
@@ -1,125 +1,126 @@
 // ==UserScript==
 // @name         B站主页动态提醒直接显示在顶栏
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  可以直接看到当前的提醒的类型, 省去鼠标移过去的麻烦...
 // @author       ipcjs
 // @match        http://www.bilibili.com/
 // @grant        none
 // ==/UserScript==
 
-(function() {
-	'use strict';
-	let msgStyleHtml = `
+(function () {
+    'use strict';
+    let msgStyleHtml = `
 #dyn_wnd {
-display: block!important;
-opacity: 1!important;
-background-color: transparent;
-
-left:-470px!important;
-top: -10px!important;
-z-index: -10000;
-
-box-shadow: none;
-border: none;
+    display: block!important;
+    opacity: 1!important;
+    background-color: transparent;
+    
+    left:-470px!important;
+    top: -10px!important;
+    z-index: -10000;
+    
+    box-shadow: none;
+    border: none;
 }
 #dyn_wnd > .dyn_menu > .menu > .line {
-left: 5px;
+    left: 5px;
 }
 `;
-	let msgHideListStyleHtml = `
+    let msgHideListStyleHtml = `
 #dyn_wnd > .dyn_menu {
-display: block!important;
+    display: block!important;
 }
 #dyn_wnd > * {
-display: none!important;
+    display: none!important;
 }
 `;
-	let msgStyle = $('<style>').attr('id','msg_style').html(msgStyleHtml);
-	let msgHideListStyle = $('<style>').attr('id','msg_hide_list_style').html(msgHideListStyleHtml);
-	let head = $('head');
-	let dynamic = $('#i_menu_msg_btn');
-	head.append(msgStyle, msgHideListStyle);
+    let msgStyle = $('<style>').attr('id', 'msg_style').html(msgStyleHtml);
+    let msgHideListStyle = $('<style>').attr('id', 'msg_hide_list_style').html(msgHideListStyleHtml);
+    let head = $('head');
+    let dynamic = $('#i_menu_msg_btn');
+    head.append(msgStyle, msgHideListStyle);
 
-	let enterFromI = false, showMsgListTimeoutId;
-	dynamic.find('> .i-link').hover(function (){
-		enterFromI = true;
-		// console.log('i enter');
-	}, function(){
-		// console.log('i exit');
-	});
-	dynamic.hover(function (){
-		if(enterFromI){
-			msgStyle.remove();
-			msgHideListStyle.remove();
-		} else {
-			showMsgListTimeoutId = setTimeout(msgHideListStyle.remove.bind(msgHideListStyle), 300); // 使用bind, 绑定this
-			// msgHideListStyle.remove();
-		}
-		console.log('enter');
-	}, function(){
-		if(enterFromI){
-			head.append(msgStyle, msgHideListStyle);
-			enterFromI = false;
-		} else {
-			clearTimeout(showMsgListTimeoutId);
-			msgHideListStyle.appendTo(head);
-		}
-		console.log('exit');
-	});
-	
-	function hoverToPopupMsg(){
-		function autoClick(){
-			if(!enterFromI) {
-				$(this).click();
-				if ($(this).attr('mode') === window.defaultDynObj.params.type){
-					var b = window.defaultDynObj;
-					b.target.attr("loaded") || (b.initMenu(), b.init(), b.target.attr("loaded", 1));// 加载默认的视频动态
-				}
-			}
-			// console.log('click');
-		}
-		let timeoutId;
-		dynamic.find('.dyn_menu > .menu > ul > li').mouseenter(function(){
-			timeoutId = setTimeout(autoClick.bind(this), 100);
-		}).mouseleave(function(){
-			clearTimeout(timeoutId);
-		});
-	}
+    let enterFromI = false, showMsgListTimeoutId;
+    dynamic.find('> .i-link').hover(function () {
+        enterFromI = true;
+        // console.log('i enter');
+    }, function () {
+        // console.log('i exit');
+    });
+    dynamic.hover(function () {
+        if (enterFromI) {
+            msgStyle.remove();
+            msgHideListStyle.remove();
+        } else {
+            showMsgListTimeoutId = setTimeout(msgHideListStyle.remove.bind(msgHideListStyle), 300); // 使用bind, 绑定this
+            // msgHideListStyle.remove();
+        }
+        // console.log('enter');
+    }, function () {
+        if (enterFromI) {
+            head.append(msgStyle, msgHideListStyle);
+            enterFromI = false;
+        } else {
+            clearTimeout(showMsgListTimeoutId);
+            msgHideListStyle.appendTo(head);
+        }
+        // console.log('exit');
+    });
 
-	/** 使顶栏的中的"游戏中心"/"直播"等条目的详细内容的窗口延时弹出 */
-	function delayPopupInfoOnTop(){
-		jQuery.fn.getEvents = function() {
-			if (typeof(jQuery._data) == 'function') {
-				return jQuery._data(this.get(0), 'events') || {};
-			} else if (typeof(this.data) == 'function') { // jQuery version < 1.7.?
-				return this.data('events') || {};
-			}
-			return {};
-		};
-		let frameItems = $(".z_top .z_top_nav [hasframe]");
-		console.log(frameItems);
-		frameItems.each(function(){
-			let item = $(this);
-			let mouseoverEvents = item.getEvents().mouseover;
-			for(let event of mouseoverEvents){
-				if (event.handler && event.namespace === ''){ // 命名空间为空的事件, 对应弹出详细内容的事件
-					let timeoutId;
-					item.off('mouseenter', event.handler); // 先移除该事件
-					// console.log(item, event);
-					item.mouseenter(function(){
-						timeoutId = setTimeout(event.handler.bind(this), 300); // 重新添加延迟执行的事件
-					}).mouseleave(function(){
-						clearTimeout(timeoutId);
-					});
-					break;
-				}
-			}
-		});
-	}
+    function hoverToPopupMsg() {
+        function autoClick() {
+            if (!enterFromI) {
+                $(this).click();
+                if ($(this).attr('mode') === window.defaultDynObj.params.type) {
+                    var b = window.defaultDynObj;
+                    b.target.attr("loaded") || (b.initMenu(), b.init(), b.target.attr("loaded", 1));// 加载默认的视频动态
+                }
+            }
+            // console.log('click');
+        }
 
-	$(window).load(function(){
-		hoverToPopupMsg();
-		delayPopupInfoOnTop();
-	});
+        let timeoutId;
+        dynamic.find('.dyn_menu > .menu > ul > li').mouseenter(function () {
+            timeoutId = setTimeout(autoClick.bind(this), 100);
+        }).mouseleave(function () {
+            clearTimeout(timeoutId);
+        });
+    }
+
+    function delayMouseover(selector, namespace) {
+        let frameItems = $(selector);
+        console.log(frameItems);
+        frameItems.each(function () {
+            let item = $(this);
+            let mouseoverEvents = item.getEvents().mouseover;
+            for (let event of mouseoverEvents) {
+                if (event.handler && event.namespace === namespace) { // 命名空间为空的事件, 对应弹出详细内容的事件
+                    let timeoutId;
+                    item.off('mouseenter', event.handler); // 先移除该事件
+                    // console.log(item, event);
+                    item.mouseenter(function () {
+                        timeoutId = setTimeout(event.handler.bind(this), 300); // 重新添加延迟执行的事件
+                    }).mouseleave(function () {
+                        clearTimeout(timeoutId);
+                    });
+                    break;
+                }
+            }
+        });
+    }
+
+    $(window).load(function () {
+        jQuery.fn.getEvents = function () {
+            if (typeof(jQuery._data) == 'function') {
+                return jQuery._data(this.get(0), 'events') || {};
+            } else if (typeof(this.data) == 'function') { // jQuery version < 1.7.?
+                return this.data('events') || {};
+            }
+            return {};
+        };
+        delayMouseover('.z_top .z_top_nav [hasframe]', ''); // 使顶栏的中的"游戏中心"/"直播"等条目的详细内容的窗口延时弹出
+        delayMouseover('#i_menu_become_vip', ''); // 使"我的大会员"延时弹出
+        hoverToPopupMsg();
+    });
 })();
