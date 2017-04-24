01a283e8e7d34f76b4a1fc005c624aa548a4fb47
ipcjs <gipcjs@gmail.com>
2017-04-24 23:47:12 +0800

    update version

--- bilibili_bangumi_aera_limit_hack.user.js
+++ bilibili_bangumi_aera_limit_hack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         解除B站区域限制
 // @namespace    http://tampermonkey.net/
-// @version      5.0.4
+// @version      5.0.5
 // @description  通过替换获取视频地址接口的方式, 实现解除B站区域限制; 只对HTML5播放器生效; 只支持bangumi.bilibili.com域名下的番剧视频;
 // @author       ipcjs
 // @require      https://static.hdslb.com/js/md5.js
