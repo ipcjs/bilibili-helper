45c96de04c858ee0e650ee1fc87bd6b08727f2cb
ipcjs <gipcjs@gmail.com>
2017-05-28 20:01:05 +0800

    修改boism的样式

---
+++ boism_style.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Boism Style
+// @namespace    https://github.com/ipcjs
+// @version      0.1
+// @description  目前黑白两条Bar太丑的, 实在看不下去
+// @author       ipcjs
+// @match        https://boism.org/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+'use strict';
+
+let css = `
+#header > .content-wrapper {
+	display: none;
+}
+
+#header {
+    background: none;
+}`;
+
+GM_addStyle(css);
