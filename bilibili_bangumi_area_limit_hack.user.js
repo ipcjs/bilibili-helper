b2bb3ff17778668904eb57d1d545387b1a115f85
ipcjs <gipcjs@gmail.com>
2017-08-06 20:20:02 +0800

    增加版本号

--- bilibili_bangumi_area_limit_hack.user.js
+++ bilibili_bangumi_area_limit_hack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         解除B站区域限制
 // @namespace    http://tampermonkey.net/
-// @version      5.3.4
+// @version      5.3.5
 // @description  通过替换获取视频地址接口的方式, 实现解除B站区域限制; 只对HTML5播放器生效; 只支持番剧视频;
 // @author       ipcjs
 // @require      https://static.hdslb.com/js/md5.js
