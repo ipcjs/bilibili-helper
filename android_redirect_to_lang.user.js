53c1aa7542165f41773d21b23fa612a9cc0fdde5
ipcjs <gipcjs@gmail.com>
2018-03-22 02:39:37 +0800

    保证页面中的链接也是hl=en结尾

--- android_redirect_to_lang.user.js
+++ android_redirect_to_lang.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         developer.android.com redirect to lang
 // @namespace    https://github.com/ipcjs/
-// @version      0.1
+// @version      0.1.1
 // @description  Android开发者官网重定向到特定语言
 // @author       ipcjs
 // @match        https://developer.android.com/*
@@ -10,9 +10,17 @@
 // ==/UserScript==
 
 'use strict';
-
-const url = new URL(location.href)
-if(!url.searchParams.get('hl')){
-    url.searchParams.set('hl', 'en')
-    location.href = url.href
+const mustEndWithLang = (item) => {
+    if (!item.href) return
+    const url = new URL(item.href)
+    if (!url.searchParams.get('hl')) {
+        url.searchParams.set('hl', 'en')
+        item.href = url.href
+    }
 }
+mustEndWithLang(location)
+window.addEventListener('DOMContentLoaded', (event) => {
+    for (let $a of document.querySelectorAll('a')) {
+        mustEndWithLang($a)
+    }
+})
