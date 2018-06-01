7d8e14454221ac127d64746db552c6aa54e12acd
ipcjs <gipcjs@gmail.com>
2018-06-02 02:31:20 +0800

    anitama

---
+++ anitama_visited_link_color.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Anitama Visited Link Color
+// @namespace    https://github.com/ipcjs
+// @version      0.0.1
+// @description  阿尼他妈访问过的链接变成灰色
+// @author       ipcjs
+// @match        *://m.anitama.cn/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+'use strict'
+GM_addStyle(`
+#list-index > a.item:visited > *, #lightbox-index a.area-title:visited > *, #list-search a.item:visited > * {
+    color: #bbb;
+}
+`)
