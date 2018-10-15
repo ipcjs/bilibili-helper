acad32c37aa07391445b9189d87014c083c2f88f
ipcjs <gipcjs@gmail.com>
2018-10-15 23:38:07 +0800

    bsnc: modify style

--- bangumi_subject_name_cn.user.js
+++ bangumi_subject_name_cn.user.js
@@ -1,16 +1,31 @@
 // ==UserScript==
 // @name         番组计划主页观看进度中文标题
 // @namespace    https://github.com/ipcjs
-// @version      0.0.3
+// @version      0.1.0
 // @description  番组计划主页观看进度中文标题
 // @author       ipcjs
 // @include      https://bgm.tv/
 // @include      https://bangumi.tv/
-// @require      https://raw.githubusercontent.com/ipcjs/bilibili-helper/user.js/ipcjs.lib.js
+// @require      https://raw.githubusercontent.com/ipcjs/bilibili-helper/8c490bbbb402f89ab63caaa13a84dc5bbb9d06f0/ipcjs.lib.js
 // @grant        none
 // ==/UserScript==
 
+const css = `
+.epGird > .tinyHeader {
+    display: flex!important;
+}
+.epGird > .tinyHeader > [data-subject-name] {
+    flex: auto;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}
+.epGird > .tinyHeader > * {
+    margin-right: 2px;
+}
+`
 ipcjs.installInto(({ log, _, html }) => {
+    document.head.appendChild(_('style', {}, css))
     const epGrids = document.querySelectorAll('.infoWrapper_tv .epGird')
     // log(epGrids.length, epGrids)
     epGrids.forEach((item) => {
