b38687080531ed878e41a45c62771d88fe2f36e4
ipcjs <gipcjs@gmail.com>
2018-11-24 22:09:39 +0800

    zmf: init

---
+++ zhihu_mobile_fuck.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Fuck ZhiHu Mobile
+// @namespace    https://github.com/ipcjs
+// @version      0.0.1
+// @description  让逼乎更美好
+// @author       ipcjs
+// @match        https://www.zhihu.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+console.log('fuck')
+GM_addStyle(`
+.DownloadGuide {
+    display: none;
+}
+`)
