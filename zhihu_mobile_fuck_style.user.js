b929a6d2e42d5c4e03e93a12c76e1266f79fa982
ipcjs <gipcjs@gmail.com>
2018-11-25 15:42:15 +0800

    zmfs: 样式调整

--- zhihu_mobile_fuck_style.user.js
+++ zhihu_mobile_fuck_style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fuck ZhiHu Mobile Style
 // @namespace    https://github.com/ipcjs
-// @version      1.0.3
+// @version      2.0.0
 // @description  日他娘的逼乎手机网页版 样式ver
 // @author       ipcjs
 // @include      https://www.zhihu.com/*
@@ -16,37 +16,49 @@ GM_addStyle(`
 .AppHeader-inner {
     width: 100%;
 }
-.SearchBar-toolWrapper {
-    width: 50px;
+.SearchBar {
+    display: none;
 }
-.SearchBar-input {
-    width: 200px;
+.AppHeader-messages, .AppHeader-notifications, .AppHeader-userInfo {
+    margin-right: 16px;
 }
-.SearchBar-input.SearchBar-focusedInput {
-    width: 400px;
+.AppHeader-navItem {
+    padding: 0 5px;
 }
-.AppHeader-userInfo {
-    margin-right: 40px;
-}
-
-.Topstory-container {
-    width: 100%;
-    padding: 0px;
-}
-.Topstory-mainColumn {
-    width: 100%;
-    /*margin: 0px;*/
+.AppHeader-nav {
+    margin-left: 16px;
+    margin-right: 16px;
 }
 
-.Question-main {
+.Topstory-container, .Question-main, .Profile-main {
     display: block;
     width: 100%;
     padding: 0px;
 }
-.Question-mainColumn {
+.Topstory-mainColumn, .Question-mainColumn, .Profile-mainColumn {
     width: 100%;
 }
-.Question-sideColumn {
+.Question-sideColumn, .Profile-sideColumn {
     width: 100%;
+}
+.QuestionHeader-content {
+    width: 100%;
+    display: block;
+}
+.QuestionHeader-main {
+    width: 100%;
+}
+.QuestionHeader-side {
+    width: 100%; 
+}
+.NumberBoard {
+    margin: auto;
+}
+.QuestionHeader {
+    min-width: inherit;
+}
+.ProfileHeader {
+    width: 100%;
+    padding: 0px;
 }
 `)
