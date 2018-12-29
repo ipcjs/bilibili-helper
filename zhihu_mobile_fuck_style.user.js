060509db2654885c81fe079565b950abc542ed6d
JiangSong <gipcjs@gmail.com>
2018-12-29 18:50:38 +0800

    zmfs: 适配firefox

--- zhihu_mobile_fuck_style.user.js
+++ zhihu_mobile_fuck_style.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
 // @name         Fuck ZhiHu Mobile Style
 // @namespace    https://github.com/ipcjs
-// @version      2.0.2
+// @version      2.0.3
 // @description  日他娘的逼乎手机网页版 样式ver; 针对电脑版进行修改，适配手机屏幕;
 // @author       ipcjs
+// @compatible   chrome
+// @compatible   firefox
 // @include      https://www.zhihu.com/*
 // @include      https://zhuanlan.zhihu.com/*
+// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @require      https://greasyfork.org/scripts/373283-ipcjs-lib-js/code/ipcjslibjs.js?version=647820
 // @grant        GM_addStyle
+// @grant        GM.addStyle
 // ==/UserScript==
 
-GM_addStyle(`
+GM.addStyle(`
 /* Header */
 .AppHeader {
     min-width: inherit;
