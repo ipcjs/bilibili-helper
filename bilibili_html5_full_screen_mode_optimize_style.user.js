8d055b9f2fe45c43dc834a7a040185e86cfe4239
ipcjs <gipcjs@gmail.com>
2018-12-30 21:37:07 +0800

    bhfsmos: 新版播放器网页全屏时控制条的位置

--- bilibili_html5_full_screen_mode_optimize_style.user.js
+++ bilibili_html5_full_screen_mode_optimize_style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili HTML5播放器网页全屏模式优化 脚本版
 // @namespace    http://tampermonkey.net/
-// @version      0.1.6.1
+// @version      1.0.0
 // @description  移植自：http://userstyles.org/styles/131642
 // @author       ipcjs
 // @include      *://www.bilibili.com/video/av*
@@ -29,50 +29,61 @@ const css = `
     width:100%!important;
   }
 
-  #bilibiliPlayer.mode-webfullscreen .bilibili-player-video-control{
-    //display: block;
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-control{
+    /*display: block;*/
     opacity: 0!important;
     transition: 0.2s;
     position:absolute;
     bottom:0px;
-  }
-  #bilibiliPlayer.mode-webfullscreen .bilibili-player-video-control:hover{
-/*     opacity: 0.7!important; */
+}
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-control:hover{
+    /*opacity: 0.7!important; */
     opacity: 1!important;
-  }
-  #bilibiliPlayer.mode-webfullscreen .bilibili-player-video-sendbar{
-    //display: block;
+}
+
+/* 新版播放器的控制条 */
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-control-wrap .bilibili-player-video-control{
+    opacity: 0!important;
+    transition: 0.2s;
+    position: relative;
+}
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-control-wrap .bilibili-player-video-control:hover{
+    opacity: 1!important;
+}
+
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-sendbar{
+    /*display: block;*/
     opacity: 0!important;
     transition: 0.2s;
     position:absolute;
     top:0px;
     width:100%!important;
-  }
+}
 
-  #bilibiliPlayer.mode-webfullscreen .bilibili-player-video-sendbar:hover{
-/*     opacity: 0.8!important; */
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-sendbar:hover{
+    /*opacity: 0.8!important; */
     opacity: 1!important;
-  }
+}
 
-  #bilibiliPlayer.mode-webfullscreen .bilibili-player-video-sendbar .bilibili-player-mode-selection-container{
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-sendbar .bilibili-player-mode-selection-container{
     height:120px;
     border-radius: 5px;
     top:100%;
-  }
+}
 
-  #bilibiliPlayer.mode-webfullscreen .bilibili-player-video-sendbar .bilibili-player-color-picker-container{
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-sendbar .bilibili-player-color-picker-container{
     height:208px;
     border-radius: 5px;
     top:100%;
-  }
+}
 
-  #bilibiliPlayer.mode-webfullscreen .bilibili-player-video-info-container{
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-info-container{
     top:40px;
-  }
+}
 
-  #bilibiliPlayer.mode-webfullscreen .bilibili-player-video-float-lastplay{
+#bilibiliPlayer.mode-webfullscreen .bilibili-player-video-float-lastplay{
     bottom:30px;
-  }`
+}`
 let style = document.createElement('style')
 style.innerHTML = css
 document.querySelector('head').appendChild(style)
