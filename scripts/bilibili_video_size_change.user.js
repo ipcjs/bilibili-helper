4df6e8ff8a32f0637d357e58eb7cd9b205bc7e80
ipcjs <gipcjs@gmail.com>
2019-01-21 00:50:00 +0800

    bvsc: 只针对特定页面启用

--- scripts/bilibili_video_size_change.user.js
+++ scripts/bilibili_video_size_change.user.js
@@ -1,18 +1,42 @@
 // ==UserScript==
 // @name         B站缩放视频大小
 // @namespace    https://github.com/ipcjs
-// @version      0.0.1
+// @version      0.0.2
 // @description  支持缩放B站视频大小
 // @author       ipcjs
 // @match        https://www.bilibili.com/bangumi/play/*
 // @grant        GM_addStyle
+// @grant        unsafeWindow
 // ==/UserScript==
 
+
+
 // todo: 添加配置页面
-GM_addStyle(`
-.bilibili-player-video video {
-    width: 50%!important;
-    margin-left: auto;
-    margin-right: auto;
+// https://www.bilibili.com/bangumi/media/md1239
+if (safeGet('unsafeWindow.__INITIAL_STATE__.epInfo.aid') == 31216455) {
+    addVideoStyle()
 }
-`)
+
+function addVideoStyle() {
+    console.log(`${GM_info.script.name} addStyle`)
+    GM_addStyle(`
+    .bilibili-player-video video {
+        width: 885px!important;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    `)
+}
+
+function safeGet(code) {
+    return eval(`
+    (()=>{
+        try{
+            return ${code}
+        }catch(e){
+            console.warn(e.toString())
+            return null
+        }
+    })()
+    `)
+}
