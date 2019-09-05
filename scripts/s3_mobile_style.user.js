dee29243585ac28beca93567302e6a9b50330024
ipcjs <gipcjs@gmail.com>
2019-09-06 02:39:34 +0800

    sms: update sytle

--- scripts/s3_mobile_style.user.js
+++ scripts/s3_mobile_style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         S3 Mobile Style
 // @namespace    https://github.com/ipcjs
-// @version      0.0.2
+// @version      0.0.3
 // @description  S3 Mobile Style
 // @author       ipcjs
 // @match        *://ac.stage3rd.com/*
@@ -26,9 +26,10 @@ function setupStyle() {
     document.documentElement.className += ` ${rootClass}`
     log('rootClass:', rootClass)
 
-    const c_detail_item_head = (num) => `.TopicBg${num}>tbody>tr>td:first-child`
-    const c_detail_item_time = (num) => `.TopicBg${num}>tbody>tr>td:nth-child(2) tr:first-child>td:first-child`
-    const c_detail_item_content = (num) => `.TopicBg${num}>tbody>tr>td:nth-child(2) tr:nth-child(2)>td`
+    const c_detail_item_head = (num) => `._forumTopicRead_asp form[name=topicReplys] .TopicBg${num}>tbody>tr>td:first-child`
+    const c_detail_item_head_owner = `._forumTopicRead_asp #main>.TopicBg1>tbody>tr>td:first-child`
+    const c_detail_item_time = (num) => `._forumTopicRead_asp .TopicBg${num}>tbody>tr>td:nth-child(2) tr:first-child>td:first-child`
+    const c_detail_item_content = (num) => `._forumTopicRead_asp .TopicBg${num}>tbody>tr>td:nth-child(2) tr:nth-child(2)>td`
     const c_list_item = (col, num) => `.rowbg${num}>td:nth-child(${col})`
     const c_list_item_title_width = '300px'
 
@@ -72,28 +73,35 @@ function setupStyle() {
         /* 详细页面 */
         ${c_detail_item_head(1)},
         ${c_detail_item_head(2)},
+        ${c_detail_item_head_owner},
         ${c_detail_item_head(1)}>div,
-        ${c_detail_item_head(2)}>div
+        ${c_detail_item_head(2)}>div,
+        ${c_detail_item_head_owner}>div
         {
             width: 50px!important;
             font-size: 0;
         }
         ${c_detail_item_head(1)} img,
-        ${c_detail_item_head(2)} img
+        ${c_detail_item_head(2)} img,
+        ${c_detail_item_head_owner} img
         {
             width: 40px;
         }
 
-        .TopicBg1 div[align=right],
-        .TopicBg2 div[align=right]
+        ._forumTopicRead_asp #main>.TopicBg1+table.TopicBg1 
+        {
+            visibility: hidden;
+        }
+        ._forumTopicRead_asp form[name=w] tr:nth-child(1)>td:nth-child(1),
+        ._forumTopicRead_asp form[name=w] tr:nth-child(2)>td:nth-child(1),
+        ._forumTopicRead_asp form[name=w] tr:nth-child(4)>td:nth-child(1),
+        ._forumTopicRead_asp form[name=w] tr:nth-child(5)>td:nth-child(1),
+        ._forumTopicRead_asp .TopicBg1 div[align=right],
+        ._forumTopicRead_asp .TopicBg2 div[align=right]
         {
             display: none;
         }
-        ._forumTopicRead_asp .PageSubject td,
-        ._forumTopicRead_asp .PageSubject+tr td,
-        ._forumTopicRead_asp .PageSubject+tr+tr td,
-        ._forumTopicRead_asp .PageSubject+tr+tr+tr td,
-        ._forumTopicRead_asp .PageSubject+tr+tr+tr+tr td,
+        form[name=w] td,
         ${c_detail_item_content(1)},
         ${c_detail_item_content(2)}
         {
