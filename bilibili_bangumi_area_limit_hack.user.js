83ce8ba9133f9abacc7037bd555360e84159136d
LoveSy <shana@zju.edu.cn>
2020-09-16 18:52:13 +0800

    Bump new ver

--- bilibili_bangumi_area_limit_hack.user.js
+++ bilibili_bangumi_area_limit_hack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         解除B站区域限制
 // @namespace    http://tampermonkey.net/
-// @version      7.9.5
+// @version      7.9.6
 // @description  通过替换获取视频地址接口的方式, 实现解除B站区域限制; 只对HTML5播放器生效;
 // @author       ipcjs
 // @supportURL   https://github.com/ipcjs/bilibili-helper/blob/user.js/bilibili_bangumi_area_limit_hack.md
