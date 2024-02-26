29ae652217fd96e288c0fb11b204a15b69e09db5
1nj0k <qwe2968@yahoo.com>
2024-02-26 18:49:46 +0800

    使用新算法转换 BV1 到 av

--- packages/unblock-area-limit/src/util/converters.ts
+++ packages/unblock-area-limit/src/util/converters.ts
@@ -31,30 +31,37 @@ export const uposMap: Record<string, string | undefined> & typeof rawUposMap = r
 
 export namespace Converters {
     // https://greasyfork.org/zh-CN/scripts/398535-bv2av/code
-    const table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF';
-    const tr: StringAnyObject = {};
-    for (var i = 0; i < 58; ++i) {
-        tr[table[i]] = i;
-    }
+    // 新算法代码实现如下：
+    // https://socialsisteryi.github.io/bilibili-API-collect/docs/misc/bvid_desc.html#javascript-typescript
+    
+    const XOR_CODE = 23442827791579n;
+    const MASK_CODE = 2251799813685247n;
+    const MAX_AID = 1n << 51n;
+    const BASE = 58n;
 
-    const s = [11, 10, 3, 8, 4, 6];
-    const xor = 177451812;
-    const add = 8728348608;
+    const data = 'FcwAPNKTMug3GV5Lj7EJnHpWsx4tb8haYeviqBz6rkCy12mUSDQX9RdoZf';
 
-    export function bv2aid(bv: string) {
-        let r = 0;
-        for (let i = 0; i < 6; ++i) {
-            r += tr[bv[s[i]]] * (58 ** i);
-        }
-        return String((r - add) ^ xor);
+    export function bv2aid(bvid: `BV1${string}`) {
+    const bvidArr = Array.from<string>(bvid);
+    [bvidArr[3], bvidArr[9]] = [bvidArr[9], bvidArr[3]];
+    [bvidArr[4], bvidArr[7]] = [bvidArr[7], bvidArr[4]];
+    bvidArr.splice(0, 3);
+    const tmp = bvidArr.reduce((pre, bvidChar) => pre * BASE + BigInt(data.indexOf(bvidChar)), 0n);
+    return String((tmp & MASK_CODE) ^ XOR_CODE);
     }
-    export function aid2bv(x: number) {
-        x = (x ^ xor) + add
-        const r = Array.from('BV1  4 1 7  ')
-        for (let i = 0; i < 6; i++) {
-            r[s[i]] = table[Math.trunc(x / (58 ** i)) % 58]
-        }
-        return r.join('')
+    
+    export function aid2bv(aid: number) {
+    const bytes = ['B', 'V', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0'];
+    let bvIndex = bytes.length - 1;
+    let tmp = (MAX_AID | BigInt(aid)) ^ XOR_CODE;
+    while (tmp > 0) {
+        bytes[bvIndex] = data[Number(tmp % BigInt(BASE))];
+        tmp = tmp / BASE;
+        bvIndex -= 1;
+    }
+    [bytes[3], bytes[9]] = [bytes[9], bytes[3]];
+    [bytes[4], bytes[7]] = [bytes[7], bytes[4]];
+    return bytes.join('') as `BV1${string}`;
     }
 
     export function xml2obj(xml: Element) {
